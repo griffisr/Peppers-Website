aa6806d30c5aebe22c61cc92657098fbe53aba03
Riley Griffis <ragriffis@gmail.com>
2022-09-07 15:28:11 -0400

    Added Analytics for Peppers Website

---
+++ analytics.js
@@ -0,0 +1,20 @@
+ // Import the functions you need from the SDKs you need
+ //import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+ //import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-analytics.js";
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+    apiKey: "AIzaSyBi86uUmf7cT6r1dpfk_weXZHxOa3iNtUI",
+  authDomain: "peppers-mexican.firebaseapp.com",
+  projectId: "peppers-mexican",
+  storageBucket: "peppers-mexican.appspot.com",
+  messagingSenderId: "1037562748140",
+  appId: "1:1037562748140:web:710fb22276f37498e959c9",
+  measurementId: "G-95QZZ9JQHY"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
