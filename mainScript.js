feb8044eae86e3e786fb1f5e914dd81539f0f9d7
Riley Griffis <ragriffis@gmail.com>
2023-01-25 16:49:08 -0500

    Updated landing page for blissfield rework

---
+++ mainScript.js
@@ -0,0 +1,76 @@
+
+  
+
+  $(document).ready(function(){
+    "use strict";
+    
+
+
+
+
+
+    var swiper = new Swiper(".mySwiper", {
+
+
+        // Navigation arrows
+      loop: true,
+      loopAdditionalSlides: 5,
+      initialSlide: 4,
+      centeredSlides: true,
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      },
+      nextButton: ".swiper-button-next",
+      prevButton: ".swiper-button-prev",
+      slidesPerView: 2.7,
+      });
+
+
+      
+
+  });
+
+
+
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    // code to run on mobile devices
+    var menuImages = document.getElementsByClassName('menu-img');
+    for (var i = 0; i < menuImages.length; i++) {
+      menuImages[i].removeEventListener('mouseover', function() {
+          new Drift(this, {
+              inlinePane: false,
+          });
+      });
+    }
+  } else {
+    // code to run on non-mobile devices
+    var menuImages = document.getElementsByClassName('menu-img');
+    for (var i = 0; i < menuImages.length; i++) {
+      menuImages[i].addEventListener('mouseover', function() {
+          new Drift(this, {
+              inlinePane: false,
+          });
+      });
+    }
+  }
+  
+
+
+
+
+  var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
